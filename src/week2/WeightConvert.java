0d25eb6313d3efb03ab7db6aec5624c5e6bafb6f
hitomak <tomaklar@hotmail.com>
2020-02-05 17:21:50 +0300

    Create WeightConvert.java
    just begin

---
+++ src/week2/WeightConvert.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class WeightConvert {
+    public static void main(String[] args) {
+        double pounds;
+        double kgtopounds=2.205;
+        System.out.println("Pounds to Kilograms Converter");
+        System.out.println("Enter the pounds");
+    }
+}
