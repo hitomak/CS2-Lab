c937dfbe8571006032871087eebf2df916e19628
hitomak <tomaklar@hotmail.com>
2020-02-07 08:08:56 +0300

    Update WeightConvert.java
    Done

--- src/week2/WeightConvert.java
+++ src/week2/WeightConvert.java
@@ -1,10 +1,14 @@
-package com.company;
+import java.util.Scanner;
 
 public class WeightConvert {
-    public static void main(String[] args) {
-        double pounds;
-        double kgtopounds=2.205;
-        System.out.println("Pounds to Kilograms Converter");
-        System.out.println("Enter the pounds");
+    public static void main(String[] args)
+    {
+        float Pounds, Kilo, Rate=2.205f;
+        Scanner measure = new Scanner(System.in);
+        System.out.println("Pounds to Kilogram Converter");
+        System.out.print("Enter Your Pounds :");
+        Pounds = measure.nextFloat();
+        Kilo = Pounds/Rate;
+        System.out.println("You entered "+Pounds+" pounds. Converted to kilogram as "+Kilo+"kg");
     }
 }
